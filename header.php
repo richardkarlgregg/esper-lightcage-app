7aea9576aca596f66a94bf286aec736a0dba4a84
richardkarlgregg <richardkarlgregg@gmail.com>
2025-03-12 21:22:23 +0000

    Misc styling tweaks

--- header.php
+++ header.php
@@ -60,9 +60,10 @@
 
 <!-- Menu Bar -->
 <div class="menu-bar bg-black border-b border-yellow-300 text-white px-4 py-1 flex items-center space-x-4">
+    <img src="<?php echo get_theme_file_uri('assets/images/esper-logo.svg'); ?>" alt="Esper Logo" class="h-4 mr-2">
     <div class="menu-item relative">
-        <button class="hover:bg-yellow-300 px-3 py-1 rounded">File</button>
-        <div class="menu-dropdown bg-black rounded shadow-lg py-1">
+        <button class="hover:bg-yellow-300 px-3 py-0 rounded">File</button>
+        <div class="menu-dropdown bg-black roundedx border border-white border-opacity-10 shadow-lg py-1">
             <button id="openJobBtn" class="w-full text-left px-4 py-2 hover:bg-yellow-300 hover:text-black flex items-center">
                 <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z"></path>
--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 
 <div class="flex" style="height: calc(100vh - 40px);">
     <!-- Left Pane: Folder Tree -->
-    <div id="sidebar" class="resizable bg-black p-4" style="width: 250px;">
+    <div id="sidebar" class="resizable bg-black border-r border-white border-opacity-10 p-4" style="width: 250px;">
         <ul id="folderTree" class="text-sm text-yellow-300 space-y-2">
         </ul>
     </div>
 
     <!-- Draggable Divider -->
-    <div id="divider" class="divider bg-yellow-300"></div>
+    <div id="divider" class="divider w-1 hover:w-4 bg-black transition hover:bg-yellow-300"></div>
 
     <!-- Right Pane: Content Area -->
-    <div id="content" class="flex-1 bg-black bg-opacity-10 overflow-auto">
+    <div id="content" class="flex-1 bg-black overflow-auto">
     </div>
 </div>
 
