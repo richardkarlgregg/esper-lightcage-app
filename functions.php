c7d6dbe1f381f3a15d413bd79f8b1ec9c0572011
richardkarlgregg <richardkarlgregg@gmail.com>
2025-03-15 09:56:30 +0000

    Add export custom post type

--- functions.php
+++ functions.php
@@ -1891,3 +1891,58 @@ function update_camera_settings_repeater_ajax() {
     }
 }
 add_action('wp_ajax_update_camera_settings_repeater', 'update_camera_settings_repeater_ajax');
+
+// Register Custom Post Type: Export
+function register_export_post_type() {
+    $labels = array(
+        'name'                  => _x('Exports', 'Post Type General Name', 'text_domain'),
+        'singular_name'         => _x('Export', 'Post Type Singular Name', 'text_domain'),
+        'menu_name'             => __('Exports', 'text_domain'),
+        'name_admin_bar'        => __('Export', 'text_domain'),
+        'archives'              => __('Export Archives', 'text_domain'),
+        'attributes'            => __('Export Attributes', 'text_domain'),
+        'parent_item_colon'     => __('Parent Export:', 'text_domain'),
+        'all_items'             => __('All Exports', 'text_domain'),
+        'add_new_item'          => __('Add New Export', 'text_domain'),
+        'add_new'               => __('Add New', 'text_domain'),
+        'new_item'              => __('New Export', 'text_domain'),
+        'edit_item'             => __('Edit Export', 'text_domain'),
+        'update_item'           => __('Update Export', 'text_domain'),
+        'view_item'             => __('View Export', 'text_domain'),
+        'view_items'            => __('View Exports', 'text_domain'),
+        'search_items'          => __('Search Export', 'text_domain'),
+        'not_found'             => __('Not found', 'text_domain'),
+        'not_found_in_trash'    => __('Not found in Trash', 'text_domain'),
+        'featured_image'        => __('Featured Image', 'text_domain'),
+        'set_featured_image'    => __('Set featured image', 'text_domain'),
+        'remove_featured_image' => __('Remove featured image', 'text_domain'),
+        'use_featured_image'    => __('Use as featured image', 'text_domain'),
+        'insert_into_item'      => __('Insert into export', 'text_domain'),
+        'uploaded_to_this_item' => __('Uploaded to this export', 'text_domain'),
+        'items_list'            => __('Exports list', 'text_domain'),
+        'items_list_navigation' => __('Exports list navigation', 'text_domain'),
+        'filter_items_list'     => __('Filter exports list', 'text_domain'),
+    );
+    $args = array(
+        'label'                 => __('Export', 'text_domain'),
+        'description'           => __('Custom post type for exports', 'text_domain'),
+        'labels'                => $labels,
+        'supports'              => array('title', 'editor', 'thumbnail', 'custom-fields'),
+        'taxonomies'            => array('category', 'post_tag'),
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'menu_position'         => 5,
+        'show_in_admin_bar'     => true,
+        'show_in_nav_menus'     => true,
+        'can_export'            => true,
+        'has_archive'           => true,
+        'exclude_from_search'   => false,
+        'publicly_queryable'    => true,
+        'capability_type'       => 'post',
+    );
+    register_post_type('export', $args);
+}
+
+add_action('init', 'register_export_post_type', 0);
